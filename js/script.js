23b3b2770838e4f6f464f138c99498e165e9f89b
Bai <ecursoinfor@gmail.com>
2025-04-21 18:53:48 +0200

    js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 const num1 = parseInt(prompt("Introduce el primer número:"));
 const num2 = parseInt(prompt("Introduce el segundo número:"));
 if( num1 > num2 ){
-    console.log("El número mayor es: " ${num1} );
+    console.log(`El número mayor es: ${num1}` );
 }
 else if( num2 > num1 ){
     console.log(`El número mayor es:  ${num2}` );
